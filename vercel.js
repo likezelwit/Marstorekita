618a977c6fded5b0308548efc79d344a1ef6ed94
Marstorekita <likezelwit@gmail.com>
2026-05-11 17:37:07 +0700

    Create vercel.js

---
+++ vercel.js
@@ -0,0 +1,29 @@
+{
+  "version": 2,
+  "functions": {
+    "api/roblox.js": {
+      "maxDuration": 10
+    }
+  },
+  "headers": [
+    {
+      "source": "/api/roblox",
+      "headers": [
+        {
+          "key": "Access-Control-Allow-Origin",
+          "value": "*"
+        },
+        {
+          "key": "Access-Control-Allow-Methods",
+          "value": "GET, POST, OPTIONS"
+        }
+      ]
+    }
+  ],
+  "rewrites": [
+    {
+      "source": "/api/roblox",
+      "destination": "/api/roblox.js"
+    }
+  ]
+}
